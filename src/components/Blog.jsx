72b2a52ed6906daa7f773577e1b5d3f9515f0066
Yash Aswale <yashaswale12@gmail.com>
2025-04-25 23:52:20 +0530

    made the blog page responsive

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -87,44 +87,46 @@ const Blog = () => {
           key={index}
           src={img}
           alt="blog"
-          className="w-24 h-24 sm:w-28 sm:h-28 md:w-32 md:h-32 rounded-xl object-cover"
+          className="w-sm h-2xs object-cover"
         />
       ))}
     </div>
   );
+  
+
 
   return (
     <>
-      <section className="w-full bg-[#181818] text-white py-16 mr-0 ml-0">
-        <div className="w-full max-w-full mx-auto flex flex-col lg:flex-row justify-between gap-12 py-16">
-          {/* Left Images */}
-          <div className="flex lg:flex-col gap-4 w-full sm:w-auto justify-center lg:justify-start h-36">
-            {renderImageGrid(leftImages)}
-          </div>
+      <section className="w-full bg-[#181818] text-white py-16">
+  <div className="w-full max-w-full mx-auto flex flex-col lg:flex-row justify-between gap-12 lg:min-h-[300px] py-16">
+    
+    {/* Left Images */}
+    <div className="hidden lg:flex lg:flex-col gap-4 justify-start">
+      {renderImageGrid(leftImages)}
+    </div>
 
-          {/* Center Text and Tags */}
-          <div className="flex-1 text-center">
-            <h2 className="text-4xl sm:text-5xl font-bold mb-8">
-              Read Our Blog
-            </h2>
-            <div className="flex flex-wrap justify-center gap-3">
-              {tags.map((tag, idx) => (
-                <span
-                  key={idx}
-                  className="bg-zinc-800 px-4 py-2 rounded-md text-sm font-medium"
-                >
-                  {tag}
-                </span>
-              ))}
-            </div>
-          </div>
+    {/* Center Text and Tags */}
+    <div className="flex-1 text-center flex flex-col justify-center">
+      <h2 className="text-4xl sm:text-5xl font-bold mb-8">Read Our Blog</h2>
+      <div className="flex flex-wrap justify-center gap-3">
+        {tags.map((tag, idx) => (
+          <span
+            key={idx}
+            className="bg-zinc-800 px-4 py-2 rounded-md text-sm font-medium"
+          >
+            {tag}
+          </span>
+        ))}
+      </div>
+    </div>
 
-          {/* Right Images */}
-          <div className="flex lg:flex-col gap-4 w-full sm:w-auto justify-center lg:justify-end">
-            {renderImageGrid(rightImages)}
-          </div>
-        </div>
-      </section>
+    {/* Right Images */}
+    <div className="hidden lg:flex lg:flex-col gap-4 justify-end">
+      {renderImageGrid(rightImages)}
+    </div>
+  </div>
+</section>
+
 
       {/* cards section  */}
       <section className="bg-[#181818] py-10 px-4">
